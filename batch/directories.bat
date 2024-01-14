3f2ed72f179f6880507ce79f108708406f56be98
linarcx <linarcx@gmail.com>
2024-01-14 15:57:10 +0100

    exp

--- batch/directories.bat
+++ batch/directories.bat
@@ -1,54 +1,40 @@
 @echo off
 
-set dirs=C:\,Home,.ssh,LocalAppData,.emacs.d,nvim,lf,mpv,Clavier.ini,Downloads,D:\;winconf,wmenu,vivre,languages,video(D:\),proramming(books),E:\,video(E:\),ConnectToPhone(Ftp)
-start /B wmenu.exe -elements "%dirs%" -prompt "Directories: " -fontName "Cascadia Code" -fontSize 8 -caseInsensitive -lineNumber 8 | more > %temp%/dirs.txt
+set dirs=C:\,%USERPROFILE%,%APPDATA%,%LOCALAPPDATA%,%TEMP%,^
+.ssh,nvim,lf,mpv,alacritty.yml,Clavier.ini,Downloads,^
+D:\,video(D:\),E:\,video(E:\),^
+winconf,wmenu,kaveh,pardis,^
+languages(books),proramming(books),connect to phone(ftp)
+
+start /B wmenu.exe -elements "%dirs%" -prompt "Directories: " -fontName "Cascadia Code PL" -fontSize 9 -caseInsensitive -lineNumber 10 | more > %temp%/dirs.txt
 
 for /f "tokens=*" %%s in (%temp%\dirs.txt) do (
-  IF "%%s"=="C:\" (
-	start C:\
-  ) ELSE IF "%%s"=="Home" (
-	start %USERPROFILE%
-  ) ELSE IF "%%s"==".ssh" (
-	start %USERPROFILE%\.ssh
-  ) ELSE IF "%%s"=="LocalAppData" (
-	start %LOCALAPPDATA%
-  ) ELSE IF "%%s"==".emacs.d" (
-	start %APPDATA%\.emacs.d
-  ) ELSE IF "%%s"=="nvim" (
-	start %LOCALAPPDATA%\nvim
-  ) ELSE IF "%%s"=="lf" (
-	start %LOCALAPPDATA%\lf
-  ) ELSE IF "%%s"=="mpv" (
-	start %LOCALAPPDATA%\mpv
-  ) ELSE IF "%%s"=="Clavier.ini" (
-	start nvim %LOCALAPPDATA%\Clavier+\Clavier.ini
-  ) ELSE IF "%%s"=="Downloads" (
-	start %userprofile%\Downloads
-  ) ELSE IF "%%s"=="D:\" (
-	start D:\
-  ) ELSE IF "%%s"=="winconf" (
-	start D:\workspace\other\winconf
-  ) ELSE IF "%%s"=="wmenu" (
-	start D:\workspace\c++\projects\wmenu
-  ) ELSE IF "%%s"=="vivre" (
-	start D:\workspace\other\vivre
-  ) ELSE IF "%%s"=="languages" (
-	start D:\books\02_languages
-  ) ELSE IF "%%s"=="video(D:\)" (
-	start D:\video
-  ) ELSE IF "%%s"=="proramming(books)" (
-	start D:\books\01_computer\programming
-  ) ELSE IF "%%s"=="E:\" (
-	start E:\
-  ) ELSE IF "%%s"=="video(E:\)" (
-	start E:\video
-  ) ELSE IF "%%s"=="ConnectToPhone(Ftp)" (
-	start explorer ftp://saeed@192.168.1.38:2121/
-  ) ELSE IF "%%s"=="E:\" (
-	start E:\
-  ) ELSE (
-	REM default case...
-  )
-)
+  if "%%s"=="C:\" ( start C:\ ) 
+  if "%%s"=="%USERPROFILE%" ( start %USERPROFILE% )
+  if "%%s"=="%APPDATA%" ( start %APPDATA% )
+  if "%%s"=="%LOCALAPPDATA%" ( start %LOCALAPPDATA% )
+  if "%%s"=="%TEMP%" ( start %TEMP% )
 
+  if "%%s"==".ssh" ( start %USERPROFILE%\.ssh )
+  if "%%s"=="nvim" ( start %LOCALAPPDATA%\nvim )
+  if "%%s"=="lf" ( start %LOCALAPPDATA%\lf )
+  if "%%s"=="mpv" ( start %LOCALAPPDATA%\mpv )
+  if "%%s"=="alacritty.yml" ( start nvim %APPDATA%\alacritty\alacritty.yml )
+  if "%%s"=="Clavier.ini" ( start nvim %USERPROFILE%\scoop\apps\clavier-plus\current\Clavier.ini )
+  if "%%s"=="Downloads" ( start %USERPROFILE%\Downloads )
+
+  if "%%s"=="D:\" ( start D:\ )
+  if "%%s"=="video(D:\)" ( start D:\video )
+  if "%%s"=="E:\" ( start E:\ )
+  if "%%s"=="video(E:\)" ( start E:\video )
+
+  if "%%s"=="winconf" ( start D:\workspace\other\winconf )
+  if "%%s"=="wmenu" ( start D:\workspace\c++\02_maintainance\wmenu )
+  if "%%s"=="kaveh" ( start D:\workspace\c\01_active\kaveh )
+  if "%%s"=="pardis" ( start D:\workspace\assembly\pardis )
+
+  if "%%s"=="languages(books)" ( start D:\books\02_languages )
+  if "%%s"=="proramming(books)" ( start D:\books\01_computer\programming )
+  if "%%s"=="connect to phone(ftp)" ( start explorer ftp://saeed@192.168.1.38:2121/ )
+)
 del "%temp%\dirs.txt"

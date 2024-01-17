c27b4e71bcb37098a307933b2d1278db1c27ad27
linarcx <linarcx@gmail.com>
2024-01-17 21:26:42 +0100

    updated

--- batch/wmenu/aliases.bat
+++ batch/wmenu/aliases.bat
@@ -1,9 +1,9 @@
 @echo off
 
-:: general
+REM General
 DOSKEY ll = ls -ltrah
 
-:: git
+REM Git
 DOSKEY gii=git init
 DOSKEY gia=git add .
 DOSKEY gis=git status
@@ -12,9 +12,8 @@ DOSKEY gib=git branch --all
 DOSKEY gid=git diff | delta
 DOSKEY gidd=git difftool
 DOSKEY gic=call functions.bat :GitCommit
+DOSKEY gip=git push --all
 
-:: DOSKEY gic=read -p "commit message: " message; git commit -m "$message"
-:: DOSKEY gip=git remote | xargs -L1 git push --all
 :: DOSKEY gr='f(){ git rm -r --cached $1; }; f'
 :: DOSKEY grsoft='f(){ git reset --hard HEAD~$1; }; f'
 :: DOSKEY grhard='f(){ git reset --soft HEAD~$1; }; f'
--- batch/wmenu/functions.bat
+++ batch/wmenu/functions.bat
@@ -1,29 +1,16 @@
-:: Check if the script is run interactively
 @echo off
 
+:: Check if the script is run interactively
 if "%cmdcmdline:~0,6%" == "cmd /c" goto :end
 
 :GitCommit
   setlocal EnableDelayedExpansion
-    :: echo hi saeed.
- 
-    :: Prompt the user for a commit message
+    REM Prompt the user for a commit message
     set /p "message=Enter commit message: "
     
-    :: Commit using the provided message
+    REM Commit using the provided message
     git commit -m "!message!"
   endlocal
 EXIT /B 0
-
-:: :firstMethodName
-::     SETLOCAL ENABLEDELAYEDEXPANSION
-::         SET firstArgumentPassedIn=%~1
-::         SET secondArgumentPassedIn=%~2
-::         
-::         ECHO %firstInstanceVariable%
-::         ECHO "The first argument passed in was %firstArgumentPassedIn%"
-::         ECHO "The second argument passed in was %secondArgumentPassedIn%"
-::     ENDLOCAL
-:: EXIT /B 0
 
 :end
